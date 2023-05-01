da0cf7dc93cac10bc0fa93ba9be69d7510236c04
gregs0n <volkovpy@mpei.ru>
2023-05-01 21:42:25 +0300

    Tests logging improved

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from tests import *
 
 def main():
-    #TestThermalCond(0, 1)
+    #TestThermalCond()
     #TestMyFunctions()
-    #TestMaterials(0, 1)
-    SingleTest(1, 1)
+    #TestMaterials()
+    SingleTest()
 
 if __name__ == '__main__':
     main()
--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ from scheme import BalanceScheme
 from enviroment import *
 from time import strftime
 
-def runtest(test: Test, show_plot: bool):
+def runtest(test: Test):
     print('-'*98)
     u0 = None
     aruntime = 0
@@ -27,79 +27,64 @@ def runtest(test: Test, show_plot: bool):
     print('\n')
     #s.trace_newt_err(show_plot=1)
     #s.trace_cg_err(show_plot=show_plot)
-    s.show_res(show_plot=show_plot)
+    s.show_res(show_plot=1)
     return runtime, aruntime
 
 step = 1/100
 cell = 5
 
-def SingleTest(debug: bool, show_plot: bool):
+def SingleTest():
     t1, t2 = 0, 0
     filename = 'logs/SingleTest_log.txt'
     test = Test(666, -4, materials[1], step, [cell, cell], int(1/(step*cell))+1)
 
-    file = open(filename, 'a')
-    print(f"\nTest '{getTestName(test)}'\nStarted - {strftime('%H:%M:%S')}", file=file)
-    t1, t2 = runtest(test, show_plot)
-    print(f"{t1:.6f}, {t2:.6f}", file=file)
-    print(f"Test's over - {strftime('%H:%M:%S')}", file=file)
-    file.close()
+    LogTest(filename, 0, test)
+    t1, t2 = runtest(test)
+    LogTest(filename, 1, t1, t2)
+    LogTest(filename, 2)
 
-def TestMaterials(debug: bool, show_plot: bool):
+def TestMaterials():
     t1, t2 = 0, 0
     filename = 'logs/TestMaterials_log.txt'
-    if debug:
-        file = open(filename, 'a')
-        print(f"\nTest started - {strftime('%H:%M:%S')}", file=file)
-        file.close()
+    LogTest(filename, 0, test)
     for i in range(6):
         test = Test(i, -4, materials[i], step, [cell, cell], int(1/(step*cell))+1)
-        t1, t2 = runtest(test, show_plot)
-        if debug:
-            file = open(filename, 'a')
-            print(f"{t1:.6f}, {t2:.6f}", file=file)
-            file.close()
-    if debug:
-        file = open(filename, 'a')
-        print(f"Test's over - {strftime('%H:%M:%S')}", file=file)
-        file.close()
+        t1, t2 = runtest(test)
+        LogTest(filename, 1, t1, t2)
+    LogTest(filename, 2)
 
 TCC = [0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10, 25, 50, 100, 250, 500]
-def TestThermalCond(debug: bool, show_plot: bool):
+def TestThermalCond():
     t1, t2 = 0, 0
     filename = 'logs/TestThermalCond_log.txt'
-    if debug:
-        file = open(filename, 'a')
-        print(f"\nTest started - {strftime('%H:%M:%S')}", file=file)
-        file.close()
+    LogTest(filename, 0, test)
     for i in range(len(TCC)):
         material = materials[1]._replace(thermal_cond=TCC[i])
         test = Test(10+i, -2, material, step, [cell, cell], int(1/(step*cell))+1)
-        t1, t2 = runtest(test, show_plot)
-        if debug:
-            file = open(filename, 'a')
-            print(f"{t1:.6f}, {t2:.6f}", file=file)
-            file.close()
-    if debug:
-        file = open(filename, 'a')
-        print(f"Test's over - {strftime('%H:%M:%S')}", file=file)
-        file.close()
+        t1, t2 = runtest(test)
+        LogTest(filename, 1, t1, t2)
+    LogTest(filename, 2)
 
-def TestMyFunctions(debug: bool, show_plot: bool):
+def TestMyFunctions():
     t1, t2 = 0, 0
     filename = 'logs/TestMyFunctions_log.txt'
-    if debug:
-        file = open(filename, 'a')
-        print(f"\nTest started - {strftime('%H:%M:%S')}", file=file)
-        file.close()
+    LogTest(filename, 0, test)
     for i in range(6):
         test = Test(40+i, i, materials[1], step, [cell, cell], int(1/(step*cell))+1)
-        t1, t2 = runtest(test, show_plot)
-        if debug:
-            file = open(filename, 'a')
-            print(f"{t1:.6f}, {t2:.6f}", file=file)
-            file.close()
-    if debug:
-        file = open(filename, 'a')
+        t1, t2 = runtest(test)
+        LogTest(filename, 1, t1, t2)
+    LogTest(filename, 2)
+
+def LogTest(fname: str, cmd: int, *args):
+    if cmd == 0:
+        file = open(fname, 'a')
         print(f"Test's over - {strftime('%H:%M:%S')}", file=file)
         file.close()
+    elif cmd == 1:
+        file = open(fname, 'a')
+        print(f"{args[0]:.6f}, {args[1]:.6f}", file=file)
+        file.close()
+    elif cmd == 2:
+        file = open(fname, 'a')
+        print(f"Test '{getTestName(args[0])}'", file=file)
+        print(f"Started - {strftime('%H:%M:%S')}", file=file)
